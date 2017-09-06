c106d18555bafd05cbb47da905a62769299a4b0a
yolossn <nssvlr@gmail.com>
2017-09-06 22:15:03 +0530

    created script for organizing images

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # wallpaper-organiser
 A simple script to organise wallpaper/images based upon the image dimension
+
+Goal:To create a python program to organize messy folder containing wallpapers into individual folders based upon the dimensions of the image.
+Python modules used:os,sys and pillow
+Output:The images are organized based upon the dimensions
---
+++ organizer.py
@@ -0,0 +1,40 @@
+import os,sys
+from PIL import Image
+if(len(sys.argv)<2):
+	print('provide the directory or enter "s" to selct the current directory')
+	sys.exit()
+if(sys.argv[1]=='s'):
+	workDir=os.getcwd();
+else:
+	homeDir='/'
+	os.chdir(homeDir)
+	#check if the user provided directory exists 
+	if not os.path.exists(sys.argv[1]):
+		print("The directory doesn't exist")
+		sys.exit()
+	else:
+		workDir=homeDir+sys.argv[1]
+		os.chdir(workDir)
+print(workDir)
+
+def organize(folderName,fileName):
+	sizeFolder=workDir+"/"+folderName
+	if not os.path.exists(sizeFolder):
+		os.makedirs(sizeFolder)
+	os.rename("{}".format(fileName),"{}/{}".format(sizeFolder,fileName)) #move files to corresponding folders
+def getDims(filename):
+	try: #to handle non image files in the directory
+		im=Image.open(filename)
+		return im.size
+	except IOError:
+		pass
+
+for root,dirs,files in os.walk(workDir):
+	for name in files:
+		dim=getDims(name)
+		if dim is not None:
+			organize("{}x{}".format(dim[0],dim[1]),name)
+
+
+
+	
